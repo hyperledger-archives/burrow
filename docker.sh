ac4b7582a6b926b54b550cf0111369410937672d
androlo <andreas@erisindustries.com>
2015-06-20 18:27:09 +0200

    temp

--- docker.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-docker build -t eris-db .
-
-# run eris-db 
-docker run --name eris-db -p 46656:46656 -p 46657:46657 -p 1337:1337 eris-db
---
+++ docker_run.sh
@@ -0,0 +1,2 @@
+# run eris-db 
+docker run --name eris-db -v  -p 46656:46656 -p 46657:46657 -p 1337:1337 eris-db
