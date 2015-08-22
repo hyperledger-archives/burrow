6ee764b08783f3927941dd1237f62863ddab809b
Ethan Buchman <ethan@coinculture.info>
2015-08-22 23:16:53 +0000

    chains new uses mintconfig and accepts csvs

--- DOCKER/chain_new.sh
+++ DOCKER/chain_new.sh
@@ -1,11 +1,22 @@
 #! /bin/bash
 
-echo "new chain: $CHAIN_ID"
+echo "your new chain, kind marmot: $CHAIN_ID"
 
 if [ "$GENERATE_GENESIS" = "true" ]; then
-	mintgen random --dir="/home/eris/.eris/blockchains/$CHAIN_ID" 1 $CHAIN_ID
-	ifExit "Error creating genesis file"
+	if [ "$CSV" = "" ]; then
+		mintgen random --dir="$CHAIN_DIR" 1 $CHAIN_ID
+		ifExit "Error creating random genesis file"
+	else
+		mintgen known --csv="$CSV" $CHAIN_ID > $CHAIN_DIR/genesis.json
+		ifExit "Error creating genesis file from csv"
+	fi
+else
+	# apparently just outputing to $CHAIN_DIR/genesis.json doesn't work so we copy
+	cat $CHAIN_DIR/genesis.json | jq .chain_id=\"$CHAIN_ID\" > genesis.json
+	cp genesis.json $CHAIN_DIR/genesis.json
 fi
+
+mintconfig $CONFIG_OPTS > $CHAIN_DIR/config.toml
 
 if [ "$RUN" = "true" ]; then
 	tendermint node
