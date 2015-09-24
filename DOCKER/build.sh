2b7fe938139b798fc6fd8f5e939c44e21ece7455
Casey Kuhlman <casey@erisindustries.com>
2015-09-24 17:53:34 +0200

    eris/erisdb -> quay.io/eris/erisdb; add a version file

--- DOCKER/build.sh
+++ DOCKER/build.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
 
-release_maj="0.10"
-release_min="0.10.3"
+if [ "$CIRCLE_BRANCH" ]
+then
+  repo=`pwd`
+else
+  base=github.com/eris-ltd/eris-db
+  repo=$GOPATH/src/$base
+fi
+branch=${CIRCLE_BRANCH:=master}
+
+release_min=$(cat $repo/version/version.go | tail -n 1 | cut -d \  -f 4 | tr -d '"')
+release_maj=$(echo $release_min | cut -d . -f 1-2)
 
 start=`pwd`
-branch=${ERISDB_BUILD_BRANCH:=master}
-base=github.com/eris-ltd/eris-db
-repo=$GOPATH/src/$base
+image_base=quay.io/eris/erisdb
 
 cd $repo
 
-# if [ "$DEV" != "true" ]; then
-# 	git checkout $branch
-# 	git pull origin
-# fi
-
-if [ "$ERISDB_BUILD_BRANCH" = "master" ]; then
-  docker build -t eris/erisdb:latest -f DOCKER/Dockerfile .
-  docker tag -f eris/erisdb:latest eris/erisdb:$release_maj
-  docker tag -f eris/erisdb:latest eris/erisdb:$release_min
+if [ "$branch" = "master" ]; then
+  docker build -t $image_base:latest -f DOCKER/Dockerfile .
+  docker tag -f $image_base:latest $image_base:$release_maj
+  docker tag -f $image_base:latest $image_base:$release_min
 else
-  docker build -t eris/erisdb:$branch -f DOCKER/Dockerfile .
+  docker build -t $image_base:$branch -f DOCKER/Dockerfile .
 fi
 
 cd $start
---
+++ version/version.go
@@ -0,0 +1,3 @@
+package version
+
+const VERSION = "0.10.4"
