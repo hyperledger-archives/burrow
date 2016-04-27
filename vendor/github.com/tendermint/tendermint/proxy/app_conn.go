063ccc31a9bf79cfd0f55df89b896098ccb53be8
Ethan Buchman <ethan@coinculture.info>
2016-04-27 18:16:19 -0400

    remove vendor

--- vendor/github.com/tendermint/tendermint/proxy/app_conn.go
+++
@@ -1,9 +0,0 @@
-package proxy
-
-import (
-	tmspcli "github.com/tendermint/tmsp/client"
-)
-
-type AppConn interface {
-	tmspcli.Client
-}
--- vendor/github.com/tendermint/tmsp/tests/test.sh
+++
@@ -1,9 +0,0 @@
-
-ROOT=$GOPATH/src/github.com/tendermint/tmsp
-cd $ROOT
-
-# test golang counter
-COUNTER_APP="counter" go run $ROOT/tests/test_counter.go
-
-# test nodejs counter
-COUNTER_APP="node ../js-tmsp/example/app.js" go run $ROOT/tests/test_counter.go
