b52c874d12482fce093311d9bfcebf7fa56fde6b
Ethan Buchman <ethan@coinculture.info>
2016-04-28 13:12:32 -0400

    add rpc tests. not finished yet

---
+++ rpc/test/helpers.go
@@ -0,0 +1,331 @@
+package rpctest
+
+import (
+	"bytes"
+	"strconv"
+	"testing"
+
+	acm "github.com/eris-ltd/eris-db/account"
+	edb "github.com/eris-ltd/eris-db/erisdb"
+	edbcli "github.com/eris-ltd/eris-db/rpc/client"
+	ctypes "github.com/eris-ltd/eris-db/rpc/core/types"
+	sm "github.com/eris-ltd/eris-db/state"
+	stypes "github.com/eris-ltd/eris-db/state/types"
+	edbapp "github.com/eris-ltd/eris-db/tmsp"
+	txs "github.com/eris-ltd/eris-db/txs"
+
+	. "github.com/tendermint/go-common"
+	"github.com/tendermint/go-crypto"
+	dbm "github.com/tendermint/go-db"
+	"github.com/tendermint/go-events"
+	"github.com/tendermint/go-p2p"
+	rpcclient "github.com/tendermint/go-rpc/client"
+	"github.com/tendermint/go-wire"
+
+	"github.com/tendermint/tendermint/config/tendermint_test"
+	nm "github.com/tendermint/tendermint/node"
+	"github.com/tendermint/tendermint/types"
+)
+
+// global variables for use across all tests
+var (
+	node              *nm.Node
+	mempoolCount      = 0
+	chainID           string
+	rpcAddr           string
+	requestAddr       string
+	websocketAddr     string
+	websocketEndpoint string
+	clientURI         *rpcclient.ClientURI
+	clientJSON        *rpcclient.ClientJSONRPC
+
+	user    = makeUsers(5) // make keys
+	clients map[string]rpcclient.Client
+)
+
+func init() {
+	tendermint_test.ResetConfig("rpc_test_client_test")
+	initGlobalVariables()
+
+	saveNewPriv()
+}
+
+// initialize config and create new node
+func initGlobalVariables() {
+	chainID = config.GetString("tm.chain_id")
+	rpcAddr = config.GetString("tm.rpc_laddr")
+	config.Set("erisdb.chain_id", chainID)
+	requestAddr = rpcAddr
+	websocketAddr = rpcAddr
+	websocketEndpoint = "/websocket"
+
+	clientURI = rpcclient.NewClientURI(requestAddr)
+	clientJSON = rpcclient.NewClientJSONRPC(requestAddr)
+
+	clients = map[string]rpcclient.Client{
+		"JSONRPC": clientJSON,
+		"HTTP":    clientURI,
+	}
+
+	// write the genesis
+	MustWriteFile(config.GetString("tm.genesis_file"), []byte(defaultGenesis), 0600)
+
+	// TODO: change consensus/state.go timeouts to be shorter
+
+	// start a node
+	ready := make(chan struct{})
+	go newNode(ready)
+	<-ready
+}
+
+// deterministic account generation, synced with genesis file in config/tendermint_test/config.go
+func makeUsers(n int) []*acm.PrivAccount {
+	accounts := []*acm.PrivAccount{}
+	for i := 0; i < n; i++ {
+		secret := ("mysecret" + strconv.Itoa(i))
+		user := acm.GenPrivAccountFromSecret(secret)
+		accounts = append(accounts, user)
+	}
+	return accounts
+}
+
+// create a new node and sleep forever
+func newNode(ready chan struct{}) {
+	stateDB := dbm.GetDB("app_state", "memdb", "")
+	genDoc, state := sm.MakeGenesisStateFromFile(stateDB, config.GetString("tm.genesis_file"))
+	state.Save()
+	buf, n, err := new(bytes.Buffer), new(int), new(error)
+	wire.WriteJSON(genDoc, buf, n, err)
+	stateDB.Set(stypes.GenDocKey, buf.Bytes())
+	if *err != nil {
+		Exit(Fmt("Unable to write gendoc to db: %v", err))
+	}
+	evsw := events.NewEventSwitch()
+	evsw.Start()
+	// create the app
+	app := edbapp.NewErisDBApp(state, evsw)
+
+	// Create & start node
+	privValidatorFile := config.GetString("tm.priv_validator_file")
+	privValidator := types.LoadOrGenPrivValidator(privValidatorFile)
+	node = nm.NewNode(privValidator, nm.GetProxyApp)
+	l := p2p.NewDefaultListener("tcp", config.GetString("tm.node_laddr"), true)
+	node.AddListener(l)
+	node.Start()
+
+	// Run the RPC server.
+	edb.StartRPC(node, app)
+	ready <- struct{}{}
+
+	// Sleep forever
+	ch := make(chan struct{})
+	<-ch
+}
+
+func saveNewPriv() {
+	// Save new priv_validator file.
+	priv := &types.PrivValidator{
+		Address: user[0].Address,
+		PubKey:  crypto.PubKeyEd25519(user[0].PubKey.(crypto.PubKeyEd25519)),
+		PrivKey: crypto.PrivKeyEd25519(user[0].PrivKey.(crypto.PrivKeyEd25519)),
+	}
+	priv.SetFile(config.GetString("tm.priv_validator_file"))
+	priv.Save()
+}
+
+//-------------------------------------------------------------------------------
+// some default transaction functions
+
+func makeDefaultSendTx(t *testing.T, typ string, addr []byte, amt int64) *txs.SendTx {
+	nonce := getNonce(t, typ, user[0].Address)
+	tx := txs.NewSendTx()
+	tx.AddInputWithNonce(user[0].PubKey, amt, nonce+1)
+	tx.AddOutput(addr, amt)
+	return tx
+}
+
+func makeDefaultSendTxSigned(t *testing.T, typ string, addr []byte, amt int64) *txs.SendTx {
+	tx := makeDefaultSendTx(t, typ, addr, amt)
+	tx.SignInput(chainID, 0, user[0])
+	return tx
+}
+
+func makeDefaultCallTx(t *testing.T, typ string, addr, code []byte, amt, gasLim, fee int64) *txs.CallTx {
+	nonce := getNonce(t, typ, user[0].Address)
+	tx := txs.NewCallTxWithNonce(user[0].PubKey, addr, code, amt, gasLim, fee, nonce+1)
+	tx.Sign(chainID, user[0])
+	return tx
+}
+
+func makeDefaultNameTx(t *testing.T, typ string, name, value string, amt, fee int64) *txs.NameTx {
+	nonce := getNonce(t, typ, user[0].Address)
+	tx := txs.NewNameTxWithNonce(user[0].PubKey, name, value, amt, fee, nonce+1)
+	tx.Sign(chainID, user[0])
+	return tx
+}
+
+//-------------------------------------------------------------------------------
+// rpc call wrappers (fail on err)
+
+// get an account's nonce
+func getNonce(t *testing.T, typ string, addr []byte) int {
+	client := clients[typ]
+	ac, err := edbcli.GetAccount(client, addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if ac == nil {
+		return 0
+	}
+	return ac.Sequence
+}
+
+// get the account
+func getAccount(t *testing.T, typ string, addr []byte) *acm.Account {
+	client := clients[typ]
+	ac, err := edbcli.GetAccount(client, addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return ac
+}
+
+// sign transaction
+func signTx(t *testing.T, typ string, tx txs.Tx, privAcc *acm.PrivAccount) txs.Tx {
+	client := clients[typ]
+	signedTx, err := edbcli.SignTx(client, tx, []*acm.PrivAccount{privAcc})
+	if err != nil {
+		t.Fatal(err)
+	}
+	return signedTx
+}
+
+// broadcast transaction
+func broadcastTx(t *testing.T, typ string, tx txs.Tx) ctypes.Receipt {
+	client := clients[typ]
+	rec, err := edbcli.BroadcastTx(client, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	mempoolCount += 1
+	return rec
+}
+
+// dump all storage for an account. currently unused
+func dumpStorage(t *testing.T, addr []byte) *ctypes.ResultDumpStorage {
+	client := clients["HTTP"]
+	resp, err := edbcli.DumpStorage(client, addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return resp
+}
+
+func getStorage(t *testing.T, typ string, addr, key []byte) []byte {
+	client := clients[typ]
+	resp, err := edbcli.GetStorage(client, addr, key)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return resp
+}
+
+func callCode(t *testing.T, client rpcclient.Client, fromAddress, code, data, expected []byte) {
+	resp, err := edbcli.CallCode(client, fromAddress, code, data)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ret := resp.Return
+	// NOTE: we don't flip memory when it comes out of RETURN (?!)
+	if bytes.Compare(ret, LeftPadWord256(expected).Bytes()) != 0 {
+		t.Fatalf("Conflicting return value. Got %x, expected %x", ret, expected)
+	}
+}
+
+func callContract(t *testing.T, client rpcclient.Client, fromAddress, toAddress, data, expected []byte) {
+	resp, err := edbcli.Call(client, fromAddress, toAddress, data)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ret := resp.Return
+	// NOTE: we don't flip memory when it comes out of RETURN (?!)
+	if bytes.Compare(ret, LeftPadWord256(expected).Bytes()) != 0 {
+		t.Fatalf("Conflicting return value. Got %x, expected %x", ret, expected)
+	}
+}
+
+// get the namereg entry
+func getNameRegEntry(t *testing.T, typ string, name string) *txs.NameRegEntry {
+	client := clients[typ]
+	entry, err := edbcli.GetName(client, name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return entry
+}
+
+//--------------------------------------------------------------------------------
+// utility verification function
+
+func checkTx(t *testing.T, fromAddr []byte, priv *acm.PrivAccount, tx *txs.SendTx) {
+	if bytes.Compare(tx.Inputs[0].Address, fromAddr) != 0 {
+		t.Fatal("Tx input addresses don't match!")
+	}
+
+	signBytes := acm.SignBytes(chainID, tx)
+	in := tx.Inputs[0] //(*types.SendTx).Inputs[0]
+
+	if err := in.ValidateBasic(); err != nil {
+		t.Fatal(err)
+	}
+	// Check signatures
+	// acc := getAccount(t, byteAddr)
+	// NOTE: using the acc here instead of the in fails; it is nil.
+	if !in.PubKey.VerifyBytes(signBytes, in.Signature) {
+		t.Fatal(txs.ErrTxInvalidSignature)
+	}
+}
+
+// simple contract returns 5 + 6 = 0xb
+func simpleContract() ([]byte, []byte, []byte) {
+	// this is the code we want to run when the contract is called
+	contractCode := []byte{0x60, 0x5, 0x60, 0x6, 0x1, 0x60, 0x0, 0x52, 0x60, 0x20, 0x60, 0x0, 0xf3}
+	// the is the code we need to return the contractCode when the contract is initialized
+	lenCode := len(contractCode)
+	// push code to the stack
+	//code := append([]byte{byte(0x60 + lenCode - 1)}, RightPadWord256(contractCode).Bytes()...)
+	code := append([]byte{0x7f}, RightPadWord256(contractCode).Bytes()...)
+	// store it in memory
+	code = append(code, []byte{0x60, 0x0, 0x52}...)
+	// return whats in memory
+	//code = append(code, []byte{0x60, byte(32 - lenCode), 0x60, byte(lenCode), 0xf3}...)
+	code = append(code, []byte{0x60, byte(lenCode), 0x60, 0x0, 0xf3}...)
+	// return init code, contract code, expected return
+	return code, contractCode, LeftPadBytes([]byte{0xb}, 32)
+}
+
+// simple call contract calls another contract
+func simpleCallContract(addr []byte) ([]byte, []byte, []byte) {
+	gas1, gas2 := byte(0x1), byte(0x1)
+	value := byte(0x1)
+	inOff, inSize := byte(0x0), byte(0x0) // no call data
+	retOff, retSize := byte(0x0), byte(0x20)
+	// this is the code we want to run (call a contract and return)
+	contractCode := []byte{0x60, retSize, 0x60, retOff, 0x60, inSize, 0x60, inOff, 0x60, value, 0x73}
+	contractCode = append(contractCode, addr...)
+	contractCode = append(contractCode, []byte{0x61, gas1, gas2, 0xf1, 0x60, 0x20, 0x60, 0x0, 0xf3}...)
+
+	// the is the code we need to return; the contractCode when the contract is initialized
+	// it should copy the code from the input into memory
+	lenCode := len(contractCode)
+	memOff := byte(0x0)
+	inOff = byte(0xc) // length of code before codeContract
+	length := byte(lenCode)
+
+	code := []byte{0x60, length, 0x60, inOff, 0x60, memOff, 0x37}
+	// return whats in memory
+	code = append(code, []byte{0x60, byte(lenCode), 0x60, 0x0, 0xf3}...)
+	code = append(code, contractCode...)
+	// return init code, contract code, expected return
+	return code, contractCode, LeftPadBytes([]byte{0xb}, 32)
+}
---
+++ rpc/test/ws_helpers.go
@@ -0,0 +1,278 @@
+package rpctest
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"testing"
+	"time"
+
+	ctypes "github.com/eris-ltd/eris-db/rpc/core/types"
+	txtypes "github.com/eris-ltd/eris-db/txs"
+	"github.com/tendermint/tendermint/types"
+
+	"github.com/tendermint/go-events"
+	client "github.com/tendermint/go-rpc/client"
+	rpctypes "github.com/tendermint/go-rpc/types"
+	"github.com/tendermint/go-wire"
+)
+
+//--------------------------------------------------------------------------------
+// Utilities for testing the websocket service
+
+// create a new connection
+func newWSClient(t *testing.T) *client.WSClient {
+	wsc := client.NewWSClient(websocketAddr, websocketEndpoint)
+	if _, err := wsc.Start(); err != nil {
+		t.Fatal(err)
+	}
+	return wsc
+}
+
+// subscribe to an event
+func subscribe(t *testing.T, wsc *client.WSClient, eventid string) {
+	if err := wsc.Subscribe(eventid); err != nil {
+		t.Fatal(err)
+	}
+}
+
+// unsubscribe from an event
+func unsubscribe(t *testing.T, wsc *client.WSClient, eventid string) {
+	if err := wsc.Unsubscribe(eventid); err != nil {
+		t.Fatal(err)
+	}
+}
+
+// wait for an event; do things that might trigger events, and check them when they are received
+// the check function takes an event id and the byte slice read off the ws
+func waitForEvent(t *testing.T, wsc *client.WSClient, eventid string, dieOnTimeout bool, f func(), check func(string, interface{}) error) {
+	// go routine to wait for webscoket msg
+	goodCh := make(chan interface{})
+	errCh := make(chan error)
+
+	// Read message
+	go func() {
+		var err error
+	LOOP:
+		for {
+			select {
+			case r := <-wsc.ResultsCh:
+				result := new(ctypes.ErisDBResult)
+				wire.ReadJSONPtr(result, r, &err)
+				if err != nil {
+					errCh <- err
+					break LOOP
+				}
+				event, ok := (*result).(*ctypes.ResultEvent)
+				if ok && event.Event == eventid {
+					goodCh <- event.Data
+					break LOOP
+				}
+			case err := <-wsc.ErrorsCh:
+				errCh <- err
+				break LOOP
+			case <-wsc.Quit:
+				break LOOP
+			}
+		}
+	}()
+
+	// do stuff (transactions)
+	f()
+
+	// wait for an event or timeout
+	timeout := time.NewTimer(10 * time.Second)
+	select {
+	case <-timeout.C:
+		if dieOnTimeout {
+			wsc.Stop()
+			t.Fatalf("%s event was not received in time", eventid)
+		}
+		// else that's great, we didn't hear the event
+		// and we shouldn't have
+	case eventData := <-goodCh:
+		if dieOnTimeout {
+			// message was received and expected
+			// run the check
+			if err := check(eventid, eventData); err != nil {
+				t.Fatal(err) // Show the stack trace.
+			}
+		} else {
+			wsc.Stop()
+			t.Fatalf("%s event was not expected", eventid)
+		}
+	case err := <-errCh:
+		t.Fatal(err)
+		panic(err) // Show the stack trace.
+
+	}
+}
+
+//--------------------------------------------------------------------------------
+
+func unmarshalResponseNewBlock(b []byte) (*types.Block, error) {
+	// unmarshall and assert somethings
+	var response rpctypes.RPCResponse
+	var err error
+	wire.ReadJSON(&response, b, &err)
+	if err != nil {
+		return nil, err
+	}
+	if response.Error != "" {
+		return nil, fmt.Errorf(response.Error)
+	}
+	// TODO
+	//block := response.Result.(*ctypes.ResultEvent).Data.(types.EventDataNewBlock).Block
+	// return block, nil
+	return nil, nil
+}
+
+func unmarshalResponseNameReg(b []byte) (*txtypes.NameTx, error) {
+	// unmarshall and assert somethings
+	var response rpctypes.RPCResponse
+	var err error
+	wire.ReadJSON(&response, b, &err)
+	if err != nil {
+		return nil, err
+	}
+	if response.Error != "" {
+		return nil, fmt.Errorf(response.Error)
+	}
+	_, val := UnmarshalEvent(*response.Result)
+	tx := txtypes.DecodeTx(val.(types.EventDataTx).Tx).(*txtypes.NameTx)
+	return tx, nil
+}
+
+func unmarshalValidateBlockchain(t *testing.T, wsc *client.WSClient, eid string) {
+	var initBlockN int
+	for i := 0; i < 2; i++ {
+		waitForEvent(t, wsc, eid, true, func() {}, func(eid string, b interface{}) error {
+			block, err := unmarshalResponseNewBlock(b.([]byte))
+			if err != nil {
+				return err
+			}
+			if i == 0 {
+				initBlockN = block.Header.Height
+			} else {
+				if block.Header.Height != initBlockN+i {
+					return fmt.Errorf("Expected block %d, got block %d", i, block.Header.Height)
+				}
+			}
+
+			return nil
+		})
+	}
+}
+
+func unmarshalValidateSend(amt int64, toAddr []byte) func(string, interface{}) error {
+	return func(eid string, b interface{}) error {
+		// unmarshal and assert correctness
+		var response rpctypes.RPCResponse
+		var err error
+		wire.ReadJSON(&response, b.([]byte), &err)
+		if err != nil {
+			return err
+		}
+		if response.Error != "" {
+			return fmt.Errorf(response.Error)
+		}
+		event, val := UnmarshalEvent(*response.Result)
+		if eid != event {
+			return fmt.Errorf("Eventid is not correct. Got %s, expected %s", event, eid)
+		}
+		tx := txtypes.DecodeTx(val.(types.EventDataTx).Tx).(*txtypes.SendTx)
+		if !bytes.Equal(tx.Inputs[0].Address, user[0].Address) {
+			return fmt.Errorf("Senders do not match up! Got %x, expected %x", tx.Inputs[0].Address, user[0].Address)
+		}
+		if tx.Inputs[0].Amount != amt {
+			return fmt.Errorf("Amt does not match up! Got %d, expected %d", tx.Inputs[0].Amount, amt)
+		}
+		if !bytes.Equal(tx.Outputs[0].Address, toAddr) {
+			return fmt.Errorf("Receivers do not match up! Got %x, expected %x", tx.Outputs[0].Address, user[0].Address)
+		}
+		return nil
+	}
+}
+
+func unmarshalValidateTx(amt int64, returnCode []byte) func(string, interface{}) error {
+	return func(eid string, b interface{}) error {
+		// unmarshall and assert somethings
+		var response rpctypes.RPCResponse
+		var err error
+		wire.ReadJSON(&response, b.([]byte), &err)
+		if err != nil {
+			return err
+		}
+		if response.Error != "" {
+			return fmt.Errorf(response.Error)
+		}
+		_, val := UnmarshalEvent(*response.Result)
+		var data = val.(txtypes.EventDataTx)
+		if data.Exception != "" {
+			return fmt.Errorf(data.Exception)
+		}
+		tx := data.Tx.(*txtypes.CallTx)
+		if !bytes.Equal(tx.Input.Address, user[0].Address) {
+			return fmt.Errorf("Senders do not match up! Got %x, expected %x",
+				tx.Input.Address, user[0].Address)
+		}
+		if tx.Input.Amount != amt {
+			return fmt.Errorf("Amt does not match up! Got %d, expected %d",
+				tx.Input.Amount, amt)
+		}
+		ret := data.Return
+		if !bytes.Equal(ret, returnCode) {
+			return fmt.Errorf("Tx did not return correctly. Got %x, expected %x", ret, returnCode)
+		}
+		return nil
+	}
+}
+
+func unmarshalValidateCall(origin, returnCode []byte, txid *[]byte) func(string, interface{}) error {
+	return func(eid string, b interface{}) error {
+		// unmarshall and assert somethings
+		var response rpctypes.RPCResponse
+		var err error
+		wire.ReadJSON(&response, b.([]byte), &err)
+		if err != nil {
+			return err
+		}
+		if response.Error != "" {
+			return fmt.Errorf(response.Error)
+		}
+		_, val := UnmarshalEvent(*response.Result)
+		var data = val.(txtypes.EventDataCall)
+		if data.Exception != "" {
+			return fmt.Errorf(data.Exception)
+		}
+		if !bytes.Equal(data.Origin, origin) {
+			return fmt.Errorf("Origin does not match up! Got %x, expected %x",
+				data.Origin, origin)
+		}
+		ret := data.Return
+		if !bytes.Equal(ret, returnCode) {
+			return fmt.Errorf("Call did not return correctly. Got %x, expected %x", ret, returnCode)
+		}
+		if !bytes.Equal(data.TxID, *txid) {
+			return fmt.Errorf("TxIDs do not match up! Got %x, expected %x",
+				data.TxID, *txid)
+		}
+		return nil
+	}
+}
+
+// Unmarshal a json event
+func UnmarshalEvent(b json.RawMessage) (string, events.EventData) {
+	var err error
+	result := new(ctypes.ErisDBResult)
+	wire.ReadJSONPtr(result, b, &err)
+	if err != nil {
+		panic(err)
+	}
+	event, ok := (*result).(*ctypes.ResultEvent)
+	if !ok {
+		return "", nil // TODO: handle non-event messages (ie. return from subscribe/unsubscribe)
+		// fmt.Errorf("Result is not type *ctypes.ResultEvent. Got %v", reflect.TypeOf(*result))
+	}
+	return event.Event, event.Data
+}
