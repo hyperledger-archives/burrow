cb1ffca54fc06facbd6aa6ec68b99e9bf146c6dc
Silas Davis <silas@erisindustries.com>
2016-12-15 10:09:48 +0000

    fix tests

--- logging/loggers/multiple_channel_logger_test.go
+++ logging/loggers/multiple_channel_logger_test.go
@@ -5,22 +5,24 @@ import (
 	"testing"
 	"time"
 
+	"github.com/eris-ltd/eris-db/logging/structure"
 	kitlog "github.com/go-kit/kit/log"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestMultipleChannelLogger(t *testing.T) {
 	boringLogger, interestingLogger := newTestLogger(), newTestLogger()
-	mcl := NewMultipleChannelLogger(map[string]kitlog.Logger{
+	mcl := kitlog.NewContext(MultipleChannelLogger(map[string]kitlog.Logger{
 		"Boring":      boringLogger,
 		"Interesting": interestingLogger,
-	})
+	}))
 	err := mcl.With("time", kitlog.Valuer(func() interface{} { return "aa" })).
-		Log("Boring", "foo", "bar")
+		Log(structure.ChannelKey, "Boring", "foo", "bar")
 	assert.NoError(t, err, "Should log without an error")
 	// Wait for channel to drain
 	time.Sleep(time.Second)
 	runtime.Gosched()
-	assert.Equal(t, []interface{}{"time", "aa", "foo", "bar"},
+	assert.Equal(t, []interface{}{"time", "aa", structure.ChannelKey, "Boring",
+		"foo", "bar"},
 		boringLogger.logLines[0])
 }
