b16f9cac23b6c3d8cbdfedba2c290a7f5561a13e
Silas Davis <silas@monax.io>
2018-01-08 12:14:39 +0000

    Account: restructure Account type into mutable and immutable interfaces, update binary package adding word160 for Address type, represent private accounts and validators, use uint64 for heights and balances, update go-crypto interfaces.
    Signed-off-by: Silas Davis <silas@monax.io>

---
+++ account/address.go
@@ -0,0 +1,95 @@
+package account
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/hyperledger/burrow/binary"
+	"github.com/tmthrgd/go-hex"
+	"golang.org/x/crypto/ripemd160"
+)
+
+type Address binary.Word160
+
+var ZeroAddress = Address{}
+
+func AddressFromBytes(addr []byte) (address Address, err error) {
+	if len(addr) != binary.Word160Length {
+		err = fmt.Errorf("slice passed as address '%X' has %d bytes but should have %d bytes",
+			addr, len(addr), binary.Word160Length)
+		// It is caller's responsibility to check for errors. If they ignore the error we'll assume they want the
+		// best-effort mapping of the bytes passed to an  address so we don't return here
+	}
+	copy(address[:], addr)
+	return
+}
+
+func AddressFromHexString(str string) (Address, error) {
+	bs, err := hex.DecodeString(str)
+	if err != nil {
+		return ZeroAddress, err
+	}
+	return AddressFromBytes(bs)
+}
+
+func MustAddressFromBytes(addr []byte) Address {
+	address, err := AddressFromBytes(addr)
+	if err != nil {
+		panic(fmt.Errorf("error reading address from bytes that caller does not expect: %s", err))
+	}
+	return address
+}
+
+func AddressFromWord256(addr binary.Word256) Address {
+	return Address(addr.Word160())
+}
+
+func (address Address) Word256() binary.Word256 {
+	return binary.Word160(address).Word256()
+}
+
+// Copy address and return a slice onto the copy
+func (address Address) Bytes() []byte {
+	addressCopy := address
+	return addressCopy[:]
+}
+
+func (address Address) String() string {
+	return hex.EncodeUpperToString(address[:])
+}
+
+func (address *Address) UnmarshalJSON(data []byte) error {
+	str := new(string)
+	err := json.Unmarshal(data, str)
+	if err != nil {
+		return err
+	}
+	_, err = hex.Decode(address[:], []byte(*str))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (address Address) MarshalJSON() ([]byte, error) {
+	return json.Marshal(hex.EncodeUpperToString(address[:]))
+}
+
+func (address *Address) UnmarshalText(text []byte) error {
+	_, err := hex.Decode(address[:], text)
+	return err
+}
+
+func (address Address) MarshalText() ([]byte, error) {
+	return ([]byte)(hex.EncodeUpperToString(address[:])), nil
+}
+
+func NewContractAddress(caller Address, sequence uint64) (newAddr Address) {
+	temp := make([]byte, 32+8)
+	copy(temp, caller[:])
+	binary.PutUint64BE(temp[32:], uint64(sequence))
+	hasher := ripemd160.New()
+	hasher.Write(temp) // does not error
+	copy(newAddr[:], hasher.Sum(nil))
+	return
+}
---
+++ account/address_test.go
@@ -0,0 +1,59 @@
+package account
+
+import (
+	"testing"
+
+	"encoding/json"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewContractAddress(t *testing.T) {
+	addr := NewContractAddress(Address{
+		233, 181, 216, 115, 19,
+		53, 100, 101, 250, 227,
+		60, 64, 108, 226, 194,
+		151, 157, 230, 11, 203,
+	}, 1)
+
+	assert.Equal(t, Address{
+		73, 234, 48, 252, 174,
+		115, 27, 222, 54, 116,
+		47, 133, 144, 21, 73,
+		245, 21, 234, 26, 50,
+	}, addr)
+}
+
+func TestAddress_MarshalJSON(t *testing.T) {
+	addr := Address{
+		73, 234, 48, 252, 174,
+		115, 27, 222, 54, 116,
+		47, 133, 144, 21, 73,
+		245, 21, 234, 26, 50,
+	}
+
+	bs, err := json.Marshal(addr)
+	assert.NoError(t, err)
+
+	addrOut := new(Address)
+	err = json.Unmarshal(bs, addrOut)
+
+	assert.Equal(t, addr, *addrOut)
+}
+
+func TestAddress_MarshalText(t *testing.T) {
+	addr := Address{
+		73, 234, 48, 252, 174,
+		115, 27, 222, 54, 116,
+		47, 133, 144, 21, 73,
+		245, 21, 234, 26, 50,
+	}
+
+	bs, err := addr.MarshalText()
+	assert.NoError(t, err)
+
+	addrOut := new(Address)
+	err = addrOut.UnmarshalText(bs)
+
+	assert.Equal(t, addr, *addrOut)
+}
