7cc0f9ed46b2bcab1afcda7188ee0abd08312fef
Silas Davis <silas@monax.io>
2018-01-08 12:17:22 +0000

    Keys: extend KeyClient interfaces and upgrade to use new Account types.
    Signed-off-by: Silas Davis <silas@monax.io>

--- keys/key_client.go
+++ keys/key_client.go
@@ -15,20 +15,47 @@
 package keys
 
 import (
+	"bytes"
 	"encoding/hex"
 	"fmt"
 
+	acm "github.com/hyperledger/burrow/account"
 	"github.com/hyperledger/burrow/logging"
 	logging_types "github.com/hyperledger/burrow/logging/types"
+	"github.com/tendermint/go-crypto"
 )
 
 type KeyClient interface {
-	// Sign needs to return the signature bytes for given message to sign
-	// and the address to sign it with.
-	Sign(signBytesString string, signAddress []byte) (signature []byte, err error)
-	// PublicKey needs to return the public key associated with a given address
-	PublicKey(address []byte) (publicKey []byte, err error)
+	// Sign returns the signature bytes for given message signed with the key associated with signAddress
+	Sign(signAddress acm.Address, message []byte) (signature crypto.Signature, err error)
+
+	// PublicKey returns the public key associated with a given address
+	PublicKey(address acm.Address) (publicKey acm.PublicKey, err error)
+
+	// Generate requests that a key be generate within the keys instance and returns the address
+	Generate(keyName string, keyType KeyType) (keyAddress acm.Address, err error)
+
+	// Returns nil if the keys isntance is health, error otherwise
+	HealthCheck() error
 }
+
+// This mirrors "github.com/monax/keys/crypto/KeyType" but since we have no use for the struct here it seems simpler
+// to replicate rather than cop an import
+type KeyType string
+
+func (kt KeyType) String() string {
+	return string(kt)
+}
+
+const (
+	KeyTypeEd25519Ripemd160         KeyType = "ed25519,ripemd160"
+	KeyTypeEd25519Ripemd160sha256           = "ed25519,ripemd160sha256"
+	KeyTypeEd25519Ripemd160sha3             = "ed25519,sha3"
+	KeyTypeSecp256k1Ripemd160               = "secp256k1,ripemd160"
+	KeyTypeSecp256k1Ripemd160sha256         = "secp256k1,ripemd160sha256"
+	KeyTypeSecp256k1Ripemd160sha3           = "secp256k1,sha3"
+	KeyTypeDefault                          = KeyTypeEd25519Ripemd160
+)
 
 // NOTE [ben] Compiler check to ensure monaxKeyClient successfully implements
 // burrow/keys.KeyClient
@@ -37,6 +64,52 @@ var _ KeyClient = (*monaxKeyClient)(nil)
 type monaxKeyClient struct {
 	rpcString string
 	logger    logging_types.InfoTraceLogger
+}
+
+type monaxSigner struct {
+	keyClient KeyClient
+	address   acm.Address
+}
+
+// Creates a Signer that assumes the address holds an Ed25519 key
+func Signer(keyClient KeyClient, address acm.Address) acm.Signer {
+	// TODO: we can do better than this and return a typed signature when we reform the keys service
+	return &monaxSigner{
+		keyClient: keyClient,
+		address:   address,
+	}
+}
+
+type keyAddressable struct {
+	publicKey acm.PublicKey
+	address   acm.Address
+}
+
+func (ka *keyAddressable) Address() acm.Address {
+	return ka.address
+}
+
+func (ka *keyAddressable) PublicKey() acm.PublicKey {
+	return ka.publicKey
+}
+
+func Addressable(keyClient KeyClient, address acm.Address) (acm.Addressable, error) {
+	pubKey, err := keyClient.PublicKey(address)
+	if err != nil {
+		return nil, err
+	}
+	return &keyAddressable{
+		address:   address,
+		publicKey: pubKey,
+	}, nil
+}
+
+func (ms *monaxSigner) Sign(messsage []byte) (crypto.Signature, error) {
+	signature, err := ms.keyClient.Sign(ms.address, messsage)
+	if err != nil {
+		return crypto.Signature{}, err
+	}
+	return signature, nil
 }
 
 // monaxKeyClient.New returns a new monax-keys client for provided rpc location
@@ -50,34 +123,59 @@ func NewBurrowKeyClient(rpcString string, logger logging_types.InfoTraceLogger) 
 
 // Monax-keys client Sign requests the signature from BurrowKeysClient over rpc for the given
 // bytes to be signed and the address to sign them with.
-func (monaxKeys *monaxKeyClient) Sign(signBytesString string, signAddress []byte) (signature []byte, err error) {
+func (monaxKeys *monaxKeyClient) Sign(signAddress acm.Address, message []byte) (crypto.Signature, error) {
 	args := map[string]string{
-		"msg":  signBytesString,
-		"hash": signBytesString, // TODO:[ben] backwards compatibility
-		"addr": fmt.Sprintf("%X", signAddress),
+		"msg":  hex.EncodeToString(message),
+		"addr": signAddress.String(),
 	}
 	sigS, err := RequestResponse(monaxKeys.rpcString, "sign", args, monaxKeys.logger)
 	if err != nil {
-		return
+		return crypto.Signature{}, err
 	}
 	sigBytes, err := hex.DecodeString(sigS)
 	if err != nil {
-		return
+		return crypto.Signature{}, err
 	}
-	return sigBytes, err
+	return crypto.SignatureEd25519FromBytes(sigBytes), err
 }
 
 // Monax-keys client PublicKey requests the public key associated with an address from
 // the monax-keys server.
-func (monaxKeys *monaxKeyClient) PublicKey(address []byte) (publicKey []byte, err error) {
+func (monaxKeys *monaxKeyClient) PublicKey(address acm.Address) (acm.PublicKey, error) {
 	args := map[string]string{
-		"addr": fmt.Sprintf("%X", address),
+		"addr": address.String(),
 	}
 	pubS, err := RequestResponse(monaxKeys.rpcString, "pub", args, monaxKeys.logger)
 	if err != nil {
-		return
+		return acm.PublicKey{}, err
 	}
-	// TODO: [ben] assert that received public key results in
-	// address
-	return hex.DecodeString(pubS)
+	pubKey := crypto.PubKeyEd25519{}
+	pubKeyBytes, err := hex.DecodeString(pubS)
+	if err != nil {
+		return acm.PublicKey{}, err
+	}
+	copy(pubKey[:], pubKeyBytes)
+	if !bytes.Equal(address.Bytes(), pubKey.Address()) {
+		return acm.PublicKey{}, fmt.Errorf("public key %s maps to address %X but was returned for address %s",
+			pubKey, pubKey.Address(), address)
+	}
+	return acm.PublicKeyFromPubKey(pubKey.Wrap()), nil
+}
+
+func (monaxKeys *monaxKeyClient) Generate(keyName string, keyType KeyType) (acm.Address, error) {
+	args := map[string]string{
+		//"auth": auth,
+		"name": keyName,
+		"type": keyType.String(),
+	}
+	addr, err := RequestResponse(monaxKeys.rpcString, "gen", args, monaxKeys.logger)
+	if err != nil {
+		return acm.ZeroAddress, err
+	}
+	return acm.AddressFromHexString(addr)
+}
+
+func (monaxKeys *monaxKeyClient) HealthCheck() error {
+	_, err := RequestResponse(monaxKeys.rpcString, "name/ls", nil, monaxKeys.logger)
+	return err
 }
---
+++ keys/key_client_test.go
@@ -0,0 +1,87 @@
+package keys
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	acm "github.com/hyperledger/burrow/account"
+	"github.com/hyperledger/burrow/logging/loggers"
+	"github.com/monax/keys/monax-keys"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+//var logger, _ = lifecycle.NewStdErrLogger()
+var logger = loggers.NewNoopInfoTraceLogger()
+
+const keysHost = "localhost"
+const keysPort = "56757"
+
+var rpcString = fmt.Sprintf("http://%s:%s", keysHost, keysPort)
+
+func TestMain(m *testing.M) {
+	var err error
+	keys.KeysDir, err = ioutil.TempDir("", "key_client_test")
+	if err != nil {
+		fatalf("couldn't create temp dir: %v", err)
+	}
+	go keys.StartServer(keysHost, keysPort)
+	m.Run()
+}
+
+func TestMonaxKeyClient_Generate(t *testing.T) {
+	keyClient := NewBurrowKeyClient(rpcString, logger)
+	addr, err := keyClient.Generate("I'm a lovely hat", KeyTypeEd25519Ripemd160)
+	assert.NoError(t, err)
+	assert.NotEqual(t, acm.ZeroAddress, addr)
+}
+
+func TestMonaxKeyClient_PublicKey(t *testing.T) {
+	keyClient := NewBurrowKeyClient(rpcString, logger)
+	addr, err := keyClient.Generate("I'm a lovely hat", KeyTypeEd25519Ripemd160)
+	assert.NoError(t, err)
+	pubKey, err := keyClient.PublicKey(addr)
+	assert.Equal(t, addr[:], pubKey.Address())
+}
+
+func TestMonaxKeyClient_PublicKey_NonExistent(t *testing.T) {
+	keyClient := NewBurrowKeyClient(rpcString, logger)
+	_, err := keyClient.PublicKey(acm.Address{8, 7, 6, 222})
+	assert.Error(t, err)
+}
+
+func TestMonaxKeyClient_Sign(t *testing.T) {
+	keyClient := NewBurrowKeyClient(rpcString, logger)
+	addr, err := keyClient.Generate("I'm a lovely hat", KeyTypeEd25519Ripemd160)
+	require.NoError(t, err)
+	pubKey, err := keyClient.PublicKey(addr)
+	assert.NoError(t, err)
+	message := []byte("I'm a hat, a hat, a hat")
+	signature, err := keyClient.Sign(addr, message)
+	assert.NoError(t, err)
+	assert.True(t, pubKey.VerifyBytes(message, signature), "signature should verify message")
+}
+
+func TestMonaxKeyClient_HealthCheck(t *testing.T) {
+	deadKeyClient := NewBurrowKeyClient("http://localhost:99999", logger)
+	assert.NotNil(t, deadKeyClient.HealthCheck())
+	keyClient := NewBurrowKeyClient(rpcString, logger)
+	assert.Nil(t, keyClient.HealthCheck())
+}
+
+func TestPublicKeyAddressAgreement(t *testing.T) {
+	keyClient := NewBurrowKeyClient(rpcString, logger)
+	addr, err := keyClient.Generate("I'm a lovely hat", KeyTypeEd25519Ripemd160)
+	require.NoError(t, err)
+	pubKey, err := keyClient.PublicKey(addr)
+	addrOut := pubKey.Address()
+	require.NoError(t, err)
+	assert.Equal(t, addr, addrOut)
+}
+
+func fatalf(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, a...)
+	os.Exit(1)
+}
