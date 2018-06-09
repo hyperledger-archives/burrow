f29a418b0af69741d8c21f8f1cab6b22696fd6d3
Casey Kuhlman <casey@monax.io>
2018-06-09 11:34:50 +0100

    upgrade bos so tests will pass
    Signed-off-by: Casey Kuhlman <casey@monax.io>

--- scripts/deps/bos.sh
+++ scripts/deps/bos.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # The git revision of Bosmarmot/bos we will build and install into ./bin/ for integration tests
-echo "29cf8c75cb5cbfe068468736eecd1c391f8b6c44"
+echo "86a3a0879782bec0a9f300d69a542fbbe3737fa9"
