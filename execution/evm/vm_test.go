ac7cfa86311a842f4549cc3bf27afa33b4e83655
greg <greg.hill@monax.io>
2018-10-08 13:03:13 +0100

    change error code
    Signed-off-by: greg <greg.hill@monax.io>

--- execution/evm/vm_test.go
+++ execution/evm/vm_test.go
@@ -739,12 +739,12 @@ func TestStaticCall(t *testing.T) {
 
 		for _, nestedError := range ourVm.nestedCallErrors {
 			err = nil
-			if nestedError.NestedError.ErrorCode() == errors.ErrorCodeInvalidStateChange {
+			if nestedError.NestedError.ErrorCode() == errors.ErrorCodeIllegalWrite {
 				err = nestedError.NestedError.ErrorCode()
 				break
 			}
 		}
-		assert.Error(t, err, errors.ErrorCodeInvalidStateChange, "Expected static call violation")
+		assert.Error(t, err, errors.ErrorCodeIllegalWrite, "Expected static call violation")
 	}
 
 	cache := state.NewCache(newAppState())
@@ -765,12 +765,12 @@ func TestStaticCall(t *testing.T) {
 
 	for _, nestedError := range ourVm.nestedCallErrors {
 		err = nil
-		if nestedError.NestedError.ErrorCode() == errors.ErrorCodeInvalidStateChange {
+		if nestedError.NestedError.ErrorCode() == errors.ErrorCodeIllegalWrite {
 			err = nestedError.NestedError.ErrorCode()
 			break
 		}
 	}
-	assert.Error(t, err, errors.ErrorCodeInvalidStateChange, "Expected static call violation")
+	assert.Error(t, err, errors.ErrorCodeIllegalWrite, "Expected static call violation")
 
 }
 
