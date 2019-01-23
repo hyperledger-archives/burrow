f71deb1ad33918c9e31acdf3a0a1f2c6afffb825
Silas Davis <silas@monax.io>
2019-01-23 11:39:39 +0000

    Ensure bytes sequences intended for storage as utf8 strings are valid utf8 bytes
    Signed-off-by: Silas Davis <silas@monax.io>

--- vent/service/rowbuilder.go
+++ vent/service/rowbuilder.go
@@ -1,9 +1,11 @@
 package service
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"strings"
+	"unicode/utf8"
 
 	"github.com/hyperledger/burrow/execution/evm/abi"
 	"github.com/hyperledger/burrow/execution/exec"
@@ -49,8 +51,8 @@ func buildEventData(spec types.EventDefinition, parser *sqlsol.Parser, event *ex
 	for k, v := range decodedData {
 		if deleteFilterLength > 0 {
 			if k == deleteFilter[0] {
-				if bytes, ok := v.(*[]byte); ok {
-					str := strings.Trim(string(*bytes), "\x00")
+				if bs, ok := v.(*[]byte); ok {
+					str := sanitiseBytesForString(*bs, l)
 					if str == deleteFilter[1] {
 						rowAction = types.ActionDelete
 					}
@@ -59,8 +61,8 @@ func buildEventData(spec types.EventDefinition, parser *sqlsol.Parser, event *ex
 		}
 		if column, err := parser.GetColumn(spec.TableName, k); err == nil {
 			if column.BytesToString {
-				if bytes, ok := v.(*[]byte); ok {
-					str := strings.Trim(string(*bytes), "\x00")
+				if bs, ok := v.(*[]byte); ok {
+					str := sanitiseBytesForString(*bs, l)
 					row[column.Name] = interface{}(str)
 					continue
 				}
@@ -74,7 +76,6 @@ func buildEventData(spec types.EventDefinition, parser *sqlsol.Parser, event *ex
 
 // buildBlkData builds block data from block stream
 func buildBlkData(tbls types.EventTables, block *exec.BlockExecution) (types.EventDataRow, error) {
-
 	// a fresh new row to store column/value data
 	row := make(map[string]interface{})
 
@@ -143,4 +144,50 @@ func buildTxData(tbls types.EventTables, txe *exec.TxExecution) (types.EventData
 	}
 
 	return types.EventDataRow{Action: types.ActionUpsert, RowData: row}, nil
+}
+
+func sanitiseBytesForString(bs []byte, l *logger.Logger) string {
+	str, err := UTF8StringFromBytes(bs)
+	if err != nil {
+		l.Error("msg", "buildEventData() received invalid bytes for utf8 string - proceeding with sanitised version",
+			"err", err)
+	}
+	// The only null bytes in utf8 are for the null code point/character so this is fine in general
+	return strings.Trim(str, "\x00")
+}
+
+// Checks whether the bytes passed are valid utf8 string bytes. If they are not returns a sanitised string version of the
+// bytes with offending sequences replaced by the utf8 replacement/error rune and an error indicating the offending
+// byte sequences and their position. Note: always returns a valid string regardless of error.
+func UTF8StringFromBytes(bs []byte) (string, error) {
+	// Provide fast path for good strings
+	if utf8.Valid(bs) {
+		return string(bs), nil
+	}
+	buf := new(bytes.Buffer)
+	var runeErrs []string
+	// This loops over runs (code points and unlike range of string gives us index of code point (i.e. utf8 char)
+	// not bytes, which we want for error message
+	var offset int
+	// Iterate over character indices (not byte indices)
+	for i := 0; i < len(bs); i++ {
+		r, n := utf8.DecodeRune(bs[offset:])
+		buf.WriteRune(r)
+		if r == utf8.RuneError {
+			runeErrs = append(runeErrs, fmt.Sprintf("0x% X (at index %d)", bs[offset:offset+n], i))
+		}
+		offset += n
+	}
+	str := buf.String()
+	errHeader := fmt.Sprintf("bytes purported to represent the string '%s'", str)
+	switch len(runeErrs) {
+	case 0:
+		// should not happen
+		return str, fmt.Errorf("bytes appear to be invalid utf8 but do not contain invalid code points")
+	case 1:
+		return str, fmt.Errorf("%s contain invalid utf8 byte sequence: %s", errHeader, runeErrs[0])
+	default:
+		return str, fmt.Errorf("%s contain invalid utf8 byte sequences: %s", errHeader,
+			strings.Join(runeErrs, ", "))
+	}
 }
