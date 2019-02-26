44328face22e6563138da9478a7396fb26727b1f
Silas Davis <silas@monax.io>
2019-02-26 16:50:01 +0000

    Fix dev builds
    Signed-off-by: Silas Davis <silas@monax.io>

--- scripts/release_dev.sh
+++ scripts/release_dev.sh
@@ -2,17 +2,14 @@
 
 set -e
 
+DOCKER_REPO=${DOCKER_REPO:-"hyperledger/burrow"}
+DOCKER_REPO_DEV=${DOCKER_REPO_DEV:-"quay.io/monax/burrow"}
+
 function release {
     echo "Pushing dev release to $DOCKER_REPO_DEV..."
     echo ${DOCKER_PASS_DEV} | docker login --username ${DOCKER_USER_DEV} ${CI_REGISTRY} ${DOCKER_HUB_DEV} --password-stdin
     docker tag ${DOCKER_REPO}:$(./scripts/local_version.sh) ${DOCKER_REPO_DEV}:$(./scripts/local_version.sh)
     docker push ${DOCKER_REPO_DEV}
 }
-
-# Only do a dev release outside pull requests
-if [[ -n "$CIRCLE_PULL_REQUESTS" ]]; then
-    echo "CIRCLE_PULL_REQUESTS is set to: '$CIRCLE_PULL_REQUEST' so not pushing dev release"
-    exit 0
-fi
 
 release
