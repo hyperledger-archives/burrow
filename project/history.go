b2ba3b2317db4be3b6ebc66ecbab43aef49ab1e8
Silas Davis <silas@monax.io>
2019-02-28 08:36:44 +0000

    Bump version
    Signed-off-by: Silas Davis <silas@monax.io>

--- project/history.go
+++ project/history.go
@@ -49,6 +49,16 @@ func FullVersion() string {
 var History relic.ImmutableHistory = relic.NewHistory("Hyperledger Burrow", "https://github.com/hyperledger/burrow").
 	MustDeclareReleases("",
 		``,
+		"0.24.1 - 2019-02-28",
+		`### Changed
+- [ABI] abi.EncodeFunctionCall and AbiSpec.Pack now take a variadic ...interface{} type for function arguments rather than []
+
+### Fixed
+- [Deploy] Binary files are now written atomically to prevent issue with dependency libraries being momentarily truncated when deploying in parallel
+
+### Added
+- [ABI] DecodeFunctionReturn re-exposed (formerly Packer then packer in 0.24.0) to make deploy API symmetrical
+`,
 
 		"0.24.0 - 2019-02-26",
 		`### Changed
