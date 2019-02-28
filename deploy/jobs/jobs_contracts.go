hyperledger-archives/burrow

commit 71e3bfc7c7d86d33723f5714785ecadf1ebcc762

create bin directory before we try to read it
If the bin directory does not exist yet, we warn about it before
we create it.

Signed-off-by: Sean Young <sean.young@monax.io>

deploy/jobs/jobs_contracts.go
@@ -48,11 +48,6 @@ func BuildJob(build *def.Build, binPath string, resp *compilers.Response) (resul
 	if binP == "" {
 		binP = binPath
 	}
-	if _, err := os.Stat(binP); os.IsNotExist(err) {
-		if err := os.Mkdir(binP, 0775); err != nil {
-			return "", err
-		}
-	}
 
 	for _, res := range resp.Objects {
 		switch build.Instance {
@@ -257,14 +252,6 @@ func FormulateDeployJob(deploy *def.Deploy, do *def.DeployArgs, deployScript *de
 }
 
 func DeployJob(deploy *def.Deploy, do *def.DeployArgs, script *def.Playbook, client *def.Client, txs []*payload.CallTx, contracts []*compilers.ResponseItem) (result string, err error) {
-
-	// Save
-	if _, err := os.Stat(do.BinPath); os.IsNotExist(err) {
-		if err := os.Mkdir(do.BinPath, 0775); err != nil {
-			return "", err
-		}
-	}
-
 	// saving contract
 	// additional data may be sent along with the contract
 	// these are naively added to the end of the contract code using standard

deploy/run_package.go
@@ -53,6 +53,12 @@ func RunPackage(do *def.DeployArgs, script *def.Playbook, client *def.Client) er
 		do.BinPath = filepath.Join(do.Path, "bin")
 	}
 
+	if _, err := os.Stat(do.BinPath); os.IsNotExist(err) {
+		if err := os.Mkdir(do.BinPath, 0775); err != nil {
+			return err
+		}
+	}
+
 	do.AllSpecs, err = abi.LoadPath(do.BinPath)
 	if err != nil {
 		log.Errorf("failed to load ABIs for Event parsing from %s: %v", do.BinPath, err)

commit def64c484fe73442777cee59a3c1135bd3db490c

Create bin directory for build jobs
This fixes a regression in commit 71e3bfc7: create bin directory before
we try to read it.

Signed-off-by: Sean Young <sean.young@monax.io>

deploy/jobs/jobs_contracts.go
@@ -47,6 +47,12 @@ func BuildJob(build *def.Build, binPath string, resp *compilers.Response) (resul
 		binP = binPath
 	}
 
+	if _, err := os.Stat(binP); os.IsNotExist(err) {
+		if err := os.Mkdir(binP, 0775); err != nil {
+			return "", err
+		}
+	}
+
 	for _, res := range resp.Objects {
 		switch build.Instance {
 		case "":