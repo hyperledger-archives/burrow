880790e17e0a46eafe36e596c53822c53faeca36
Gregory Hill <gregorydhill@outlook.com>
2020-03-13 15:58:50 +0000

    add default burrow ts client
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,9 +1,5 @@
-import { Burrow } from './lib/Burrow';
-import { Contract } from './lib/contracts/contract';
-import * as utils from './lib/utils/utils';
-
-export {
-  Burrow,
-  Contract,
-  utils,
-}
+export { Burrow } from './lib/burrow';
+export { Contract } from './lib/contracts/contract';
+export * from './lib/utils/utils';
+export { TxInput, CallTx } from '../proto/payload_pb'
+export { TxExecution } from '../proto/exec_pb';
