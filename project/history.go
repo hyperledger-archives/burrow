de2816cc22d1e7a23b342e25720db6a5d56b8114
Silas Davis <silas@monax.io>
2020-04-05 09:32:59 +0100

    Moved Solidity Go fixture 'solgo' functionality to `burrow compile` on CLI for external use
    Signed-off-by: Silas Davis <silas@monax.io>

--- project/history.go
+++ project/history.go
@@ -47,7 +47,10 @@ func FullVersion() string {
 // To cut a new release add a release to the front of this slice then run the
 // release tagging script: ./scripts/tag_release.sh
 var History relic.ImmutableHistory = relic.NewHistory("Hyperledger Burrow", "https://github.com/hyperledger/burrow").
-	MustDeclareReleases(
+	MustDeclareReleases("",
+		`### Added
+- [CLI] Made previously internal Solidity Go fixtures compilation available through 'burrow compile'
+`,
 		"0.30.3 - 2020-03-13",
 		`### Added
 - [TS] Default ts client interface implementation
